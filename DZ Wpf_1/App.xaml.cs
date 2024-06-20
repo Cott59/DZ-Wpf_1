fe48d98048f8f6cccb14f4bca2486fb0ef2dbb16
Ura-sapfir <Ura-sapfir@HOME-PC>
2024-06-20 15:50:46 +0500

    Добавьте файлы проекта.

---
+++ DZ Wpf_1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DZ_Wpf_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
